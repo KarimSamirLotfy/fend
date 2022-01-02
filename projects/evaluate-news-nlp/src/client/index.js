bd95a6c6a687c89f4a210ae4e79d64aee211158c
karim lotfy <karim.samir.lotfy@gmail.com>
2022-01-02 10:30:43 +0100

    working .env and scss compilation

--- projects/evaluate-news-nlp/src/client/index.js
+++ projects/evaluate-news-nlp/src/client/index.js
@@ -1,7 +1,15 @@
 import { checkForName } from './js/nameChecker'
 import { handleSubmit } from './js/formHandler'
+import "./styles/resets.scss";
+import "./styles/base.scss";
+import "./styles/footer.scss";
+import "./styles/form.scss";
+import "./styles/header.scss";
 
 console.log(checkForName);
 
-alert("I EXIST")
+// const dotenv = require("dotenv");
+// dotenv.config();
+console.log(`YOUR API KEY IS    ${process.env.API_KEY}`);
+
 console.log("CHANGE!!");
